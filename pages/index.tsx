95ce0c90fbb2f9956f0776cb0dd9ee303d938351
Chanakyha <vetrichanakyha2003@gmail.com>
2022-10-02 21:54:06 +0530

    Hero Image id Added ✅

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import Header from "../components/Header";
 
 const Home: NextPage = () => {
@@ -12,6 +11,23 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
+      <img src="./assets/hero-bg.svg" className="w-screen" />
+      <Border />
+    </div>
+  );
+};
+
+const Border = () => {
+  return (
+    <div className="flex w-1/2 mx-auto my-5 lg:my-10">
+      <div
+        className="inline w-[30%] bg-[#394B57] rounded-l-lg"
+        style={{ border: "3.5px solid #394B57" }}
+      />
+      <div
+        className="inline w-[70%] bg-[#00DCC9] rounded-r-lg"
+        style={{ border: "3.5px solid #00DCC9" }}
+      />
     </div>
   );
 };
