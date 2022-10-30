f91ad650db25ba1c5d26539dc2660af3ec80e4c5
Chanakyha <vetrichanakyha2003@gmail.com>
2022-10-30 21:10:40 +0530

    Navbar Added Successfully

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Sun Diagnostic Laboratory</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <Header />
     </div>
   );
 };
