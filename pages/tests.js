6e6c46bdc9446b228bca9bdaeb2bdc455539fd80
Chanakyha <vetrichanakyha2003@gmail.com>
2022-12-13 19:14:59 +0530

    test catalog page is done

--- pages/tests.js
+++ pages/tests.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Catalog from "../components/tests/Catalog";
 import Hero from "../components/tests/Hero";
 
 const Tests = () => {
   return (
     <div className="min-h-screen w-screen">
       <Hero />
+
+      <Catalog />
     </div>
   );
 };
