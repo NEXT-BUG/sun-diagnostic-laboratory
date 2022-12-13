c501b8e662456b27df9281ffbd609d3ffba8fe54
Chanakyha <vetrichanakyha2003@gmail.com>
2022-12-13 23:05:10 +0530

    Made the booking Page Responsive

--- pages/book/[testId].js
+++ pages/book/[testId].js
@@ -32,14 +32,14 @@ const Booking = () => {
 
   return (
     <div className="w-screen min-h-screen">
-      <div className="p-10 flex justify-between items-start font-ubuntu space-x-10">
+      <div className="p-3 pt-10 md:p-10 flex flex-col-reverse md:flex-row justify-between items-start font-ubuntu md:space-x-10">
         <SearchCatalog border shadow className="md:w-1/3" />
-        <div className="text-left w-2/3">
+        <div className="text-left md:w-2/3">
           <p className="font-bold text-4xl">{testObject?.testName}</p>
           <div className="mt-20">
             <h1 className="font-bold text-2xl">Book Now</h1>
             <div className="bg-primary h-1 w-full my-5 rounded-full" />
-            <div className="flex flex-col p-10 space-y-2">
+            <div className="flex flex-col md:p-10 space-y-2">
               <div className="flex items-center justify-between">
                 <p className="font-bold">Salutation</p>
                 <RadioGroup row>
@@ -68,7 +68,7 @@ const Booking = () => {
               <div className="flex items-center justify-between">
                 <p className="font-bold">Patient Name</p>
                 <Input
-                  sx={{ width: "20rem" }}
+                  sx={{ width: "15rem" }}
                   variant="standard"
                   label="Full Name"
                 />
@@ -76,7 +76,7 @@ const Booking = () => {
               <div className="flex items-center justify-between">
                 <p className="font-bold">Email</p>
                 <Input
-                  sx={{ width: "20rem" }}
+                  sx={{ width: "15rem" }}
                   variant="standard"
                   label="Email Address"
                 />
@@ -90,7 +90,7 @@ const Booking = () => {
                     renderInput={(params) => (
                       <Input
                         variant="standard"
-                        sx={{ width: "20rem", margin: "1rem 0" }}
+                        sx={{ width: "15rem", margin: "1rem 0" }}
                         {...params}
                       />
                     )}
@@ -100,7 +100,7 @@ const Booking = () => {
               <div className="flex items-center justify-between">
                 <p className="font-bold">Mobile Number</p>
                 <Input
-                  sx={{ width: "20rem" }}
+                  sx={{ width: "15rem" }}
                   variant="standard"
                   InputProps={{
                     startAdornment: <InputAdornment>+91</InputAdornment>,
@@ -117,7 +117,7 @@ const Booking = () => {
                     renderInput={(params) => (
                       <Input
                         variant="standard"
-                        sx={{ width: "20rem", margin: "1rem 0" }}
+                        sx={{ width: "15rem", margin: "1rem 0" }}
                         {...params}
                       />
                     )}
@@ -127,7 +127,7 @@ const Booking = () => {
               <div className="flex items-center justify-between">
                 <p className="font-bold">Selected Test</p>
                 <Input
-                  sx={{ width: "20rem" }}
+                  sx={{ width: "15rem" }}
                   variant="standard"
                   value={testObject?.testName}
                   disabled
@@ -138,11 +138,11 @@ const Booking = () => {
           <div className="mt-10">
             <h1 className="font-bold text-2xl">Home Collection</h1>
             <div className="bg-primary h-1 w-full my-5 rounded-full" />
-            <div className="flex flex-col p-10 space-y-2">
+            <div className="flex flex-col md:p-10 space-y-2">
               <div className="flex items-center justify-between">
                 <p className="font-bold">House Number</p>
                 <Input
-                  sx={{ width: "20rem" }}
+                  sx={{ width: "15rem" }}
                   variant="standard"
                   label="Full Name"
                 />
@@ -150,7 +150,7 @@ const Booking = () => {
               <div className="flex items-center justify-between">
                 <p className="font-bold">Street/Society</p>
                 <Input
-                  sx={{ width: "20rem" }}
+                  sx={{ width: "15rem" }}
                   variant="standard"
                   label="Email Address"
                 />
@@ -158,7 +158,7 @@ const Booking = () => {
               <div className="flex items-center justify-between">
                 <p className="font-bold">Mobile Number</p>
                 <Input
-                  sx={{ width: "20rem" }}
+                  sx={{ width: "15rem" }}
                   variant="standard"
                   InputProps={{
                     startAdornment: <InputAdornment>+91</InputAdornment>,
@@ -168,18 +168,23 @@ const Booking = () => {
               </div>
               <div className="flex items-center justify-between py-3">
                 <p className="font-bold">Attach a Prescription</p>
-                <input type="file" />
+                <input className="w-[15rem]" type="file" />
               </div>
               <div className="flex items-center justify-between">
                 <p className="font-bold">City Name</p>
                 <Input
-                  sx={{ width: "20rem" }}
+                  sx={{ width: "15rem" }}
                   variant="standard"
                   value="Chennai"
                   disabled
                 />
               </div>
             </div>
+          </div>
+          <div className="flex justify-between m-5">
+            <button className="px-4 text-white font-bold py-2 md:hover:scale-110 md:active:scale-75 duration-150 ease-out bg-primary rounded-md ml-auto">
+              Submit
+            </button>
           </div>
         </div>
       </div>
